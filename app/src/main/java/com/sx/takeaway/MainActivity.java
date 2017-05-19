bc11d72b491f836d6bb137a4cf213322b6a462dd
Sunxin <sunxin0630@gmail.com>
2017-05-19 16:11:42 +0800

    test

--- app/src/main/java/com/sx/takeaway/MainActivity.java
+++ app/src/main/java/com/sx/takeaway/MainActivity.java
@@ -2,6 +2,7 @@ package com.sx.takeaway;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT).show();
     }
 }
